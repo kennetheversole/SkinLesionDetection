b73df100d7b01be558e3ada887144eeb7ca52eee
Kenneth Eversole <kenneth.eversole@gmail.com>
2018-12-05 14:28:42 -0800

    Added base files

---
+++ src/main.py
@@ -0,0 +1,113 @@
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from glob import glob
+import seaborn as sns
+from PIL import Image
+np.random.seed(123)
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import confusion_matrix
+import itertools
+
+import keras
+from keras.utils.np_utils import to_categorical # used for converting labels to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras import backend as K
+import itertools
+from keras.layers.normalization import BatchNormalization
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
+
+
+#1. Function to plot model's validation loss and validation accuracy
+def plot_model_history(model_history):
+    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    # summarize history for accuracy
+    axs[0].plot(range(1,len(model_history.history['acc'])+1),model_history.history['acc'])
+    axs[0].plot(range(1,len(model_history.history['val_acc'])+1),model_history.history['val_acc'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_history.history['acc'])+1),len(model_history.history['acc'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # summarize history for loss
+    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    plt.show()
+
+
+
+
+
+base_skin_dir = os.path.join(os.getcwd(), "data")
+
+# Merging images from both folders HAM10000_images_part1.zip and HAM10000_images_part2.zip into one dictionary
+
+imageid_path_dict = {os.path.splitext(os.path.basename(x))[0]: x
+                     for x in glob(os.path.join(base_skin_dir, '*', '*.jpg'))}
+# This dictionary is useful for displaying more human-friendly labels later on
+
+lesion_type_dict = {
+    'nv': 'Melanocytic nevi',
+    'mel': 'Melanoma',
+    'bkl': 'Benign keratosis-like lesions ', 
+    'bcc': 'Basal cell carcinoma',
+    'akiec': 'Actinic keratoses',
+    'vasc': 'Vascular lesions',
+    'df': 'Dermatofibroma'
+}
+
+tile_df = pd.read_csv(os.path.join(base_skin_dir, "HAM10000_metadata.csv"), engine='python')
+
+
+
+
+# Creating New Columns for better readability
+
+# Create some new columns (path to image, human-readable name) and review them
+tile_df['path'] = tile_df['image_id'].map(imageid_path_dict.get)
+tile_df['cell_type'] = tile_df['dx'].map(lesion_type_dict.get) 
+tile_df['cell_type_idx'] = pd.Categorical(tile_df['cell_type']).codes
+tile_df.sample(5)
+
+tile_df.describe(exclude=[np.number])
+
+fig, ax1 = plt.subplots(1, 1, figsize= (10, 5))
+tile_df['cell_type'].value_counts().plot(kind='bar', ax=ax1)
+fig.savefig('data1.png', dpi=300)
+
+tile_df = tile_df.drop(tile_df[tile_df.cell_type_idx == 4].iloc[:5000].index)
+
+fig, ax1 = plt.subplots(1, 1, figsize= (10, 5))
+fig.savefig('data2.png', dpi=300)
+tile_df['cell_type'].value_counts().plot(kind='bar', ax=ax1)
+tile_df['dx_type'].value_counts().plot(kind='bar')
+
+tile_df['sex'].value_counts().plot(kind='bar')
+
+tile_df['image'] = tile_df['path'].map(lambda x: np.asarray(Image.open(x).resize((100,75))))
+
+
+n_samples = 10
+fig, m_axs = plt.subplots(7, n_samples, figsize = (4*n_samples, 3*7))
+for n_axs, (type_name, type_rows) in zip(m_axs, 
+                                         tile_df.sort_values(['cell_type']).groupby('cell_type')):
+    n_axs[0].set_title(type_name)
+    for c_ax, (_, c_row) in zip(n_axs, type_rows.sample(n_samples, random_state=2018).iterrows()):
+        c_ax.imshow(c_row['image'])
+        c_ax.axis('off')
+fig.savefig('category_samples.png', dpi=300)
+
+
